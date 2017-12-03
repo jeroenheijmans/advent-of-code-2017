48f40883f15b8e91187fb020b240f7d03211ace5
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-03 21:05:16 +0100

    Day 03 puzzle 1

--- days/03.js
+++ days/03.js
@@ -4,7 +4,7 @@
 
         puzzles:[{
             title: "Puzzle 1",
-            expectedAnswer: "final",
+            expectedAnswer: 430,
             testSets: [
                 { expectedAnswer: 0, data: 1 },
                 { expectedAnswer: 3, data: 12 },
@@ -21,33 +21,20 @@
                 { expectedAnswer: 4, data: 17 },
             ],
             getSolution: data => {
-                var result = 0;
-
                 if (data === 1) return 0;
 
-                for (let ringNr = 1; ringNr < 150; ringNr++) {
-                    let squareBase = ringNr * 2 + 1;
-                    let finalNrFromRing = Math.pow(squareBase, 2);
-                    let finalNrFromPreviousRing = Math.pow(squareBase - 2, 2);
+                let ringNr = Math.floor(Math.ceil(Math.sqrt(data))/2);
 
-                    if (data > finalNrFromPreviousRing && data <= finalNrFromRing) {
-                        result = ringNr;
+                let ringFirstNr = Math.pow(ringNr*2-1, 2) + 1;
+                let ringLastNr = Math.pow(ringNr*2+1, 2);
+                let nrOfItemsInRing = ringLastNr - ringFirstNr + 1;
+                let positionInRingFromBottomRight = data - ringFirstNr + 1;
+                let nrOfItemsPerLoop = nrOfItemsInRing / 4;
+                let positionInLoop = positionInRingFromBottomRight % nrOfItemsPerLoop;
 
-                        let maxNrOfExtraSteps = Math.floor(squareBase/2);
-                        let placeOnRing = data - finalNrFromPreviousRing;
-                        let nrOfStepsOnRing = finalNrFromRing - finalNrFromPreviousRing;
+                var bonusExpense = Math.abs(ringNr - positionInLoop);
 
-                        let maxAddition = ringNr * 2;
-                        let minAddition = ringNr;
-
-                        let a = ringNr * 2;
-                        let x = Math.abs(placeOnRing - (nrOfStepsOnRing / 2));
-
-                        return Math.abs(a - x - 1);
-                    }
-                }               
-
-                return result;
+                return ringNr + bonusExpense;
             }
         },
 
