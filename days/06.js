818d0e7c99932716e88f55a1daac943d9a8ee83b
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-06 06:30:38 +0100

    Day 06 puzzle 1

--- days/06.js
+++ days/06.js
@@ -1,17 +1,50 @@
 (function(aoc) {
     aoc.days["06"] = {
-        actualInput: [],
+        actualInput: [2,8,8,5,4,2,3,1,5,5,1,2,15,13,5,14],
 
         puzzles:[{
             title: "Puzzle 1",
-            expectedAnswer: -1,
+            expectedAnswer: 3156,
             testSets: [
-                { expectedAnswer: 0, data: [] },
+                { expectedAnswer: 5, data: [0,2,7,0] },
             ],
             getSolution: data => {
-                var result = 0;
+                let seenConfigurations = [
+                    // Ininitial config:
+                    JSON.stringify(data.slice(0))
+                ];
 
-                return result;
+                let nrOfLoops = 0;
+                
+                do {
+                    var tbl = JSON.stringify(data);
+                    let max = Math.max.apply(Math, data);
+                    let idx = data.indexOf(max);
+                    let redistribute = data[idx];
+                    data[idx] = 0;
+
+                    while (redistribute > 0) {
+                        redistribute--;
+                        idx ++;
+                        if (idx >= data.length) {
+                            idx = 0;
+                        }
+                        data[idx]++;
+                    }
+
+                    nrOfLoops++;
+
+                    let hash = JSON.stringify(data);
+
+                    if (seenConfigurations.indexOf(JSON.stringify(data)) >= 0) {
+                        return nrOfLoops;
+                    }
+
+                    seenConfigurations.push(hash);
+
+                } while (nrOfLoops < 100000);
+
+                return "NOT FOUND";
             }
         },
 
@@ -19,7 +52,7 @@
             title: "Puzzle 2",
             expectedAnswer: -1,
             testSets: [
-                { expectedAnswer: 0, data: [] },
+                { expectedAnswer: -1, data: [] },
             ],
             getSolution: data => {
                 var result = 0;
