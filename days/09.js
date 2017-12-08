fd62a175eb4d8cda0dac497d941f51bfe8ea1003
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-08 06:04:01 +0100

    Start day 08

---
+++ days/09.js
@@ -0,0 +1,31 @@
+(function(aoc) {
+    aoc.days["09"] = {
+        actualInput: [],
+
+        puzzles:[{
+            title: "Puzzle 1",
+            expectedAnswer: null,
+            testSets: [
+                { expectedAnswer: null, data: [] },
+            ],
+            getSolution: data => {
+                var result = 0;
+
+                return result;
+            }
+        },
+
+        /*{
+            title: "Puzzle 2",
+            expectedAnswer: null,
+            testSets: [
+                { expectedAnswer: null, data: [] },
+            ],
+            getSolution: data => {
+                var result = 0;
+
+                return result;
+            }
+        }*/]
+    };
+}(window.aoc = window.aoc || {days:{}}));
