f79b53029a64e125043fb786947e94edbad9deb5
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-10 09:35:53 +0100

    Refactor day 10

--- days/10.js
+++ days/10.js
@@ -1,5 +1,5 @@
 (function(aoc) {
-    function getPresentableData(list, currentPosition, length) {
+    function getPresentableData(list, currentPosition, length, skipSize) {
         let rightBoundary = currentPosition + length - 1;
 
         if (rightBoundary >= list.length) {
@@ -11,7 +11,70 @@
                 : idx === currentPosition ? `([${item}]`
                 : idx === rightBoundary ? `${item})`
                 : `${item}`
-            ).join(" ") + `  --  currentPosisition = ${currentPosition}  --  length = ${length}`;
+            ).join(" ") + `  --  currentPosisition = ${currentPosition}  --  length = ${length}  --  skip = ${skipSize}`;
+    }
+
+    function tieKnot(inputList, inputLengths, rounds = 1) {
+        let result = inputList.slice();
+        let currentPosition = 0;
+        let skipSize = 0;
+
+        for (let round = 0; round < rounds; round++) {
+            for (let i = 0; i < inputLengths.length; i++) {
+                let currentLoopLength = inputLengths[i];
+
+                // console.log(getPresentableData(result, currentPosition, currentLoopLength, skipSize));
+
+                let reversedSection = [];
+
+                for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
+                    readFrom = currentPosition + x;
+
+                    if (readFrom >= result.length) {
+                        readFrom -= result.length;
+                    }
+
+                    reversedSection.unshift(result[readFrom]);
+                }
+
+                for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
+                    readFrom = currentPosition + x;
+
+                    if (readFrom >= result.length) {
+                        readFrom -= result.length;
+                    }
+
+                    result[readFrom] = reversedSection[x];
+                }
+
+                currentPosition += currentLoopLength + skipSize;
+                skipSize++;
+
+                while (currentPosition >= result.length) {
+                    currentPosition -= result.length;
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    function getDenseHash(sparseHash) {
+        let result = [];
+
+        for (let blockNr = 0; blockNr < 16; blockNr++) {
+            let block = sparseHash.slice(blockNr * 16, (blockNr + 1) * 16);
+            result[blockNr] = block.reduce((a,b) => a ^ b);
+        }
+
+        return result;
+    }
+    
+    function getHexForDenseHash(denseHash) {
+        return denseHash.map(digit => {
+            let result = digit.toString(16);
+            return result.length === 1 ? `0${result}` : result;
+        }).join("");
     }
 
     aoc.days["10"] = {
@@ -24,54 +87,14 @@
                 { expectedAnswer: 12, data: { list: _.range(0,5), lengths: [3, 4, 1, 5]} },
             ],
             getSolution: data => {
-                let currentPosition = 0;
-                let skipSize = 0;
-
-                let inputList = data.list;
-                let inputLengths = data.lengths;
-
-                for (let i = 0; i < inputLengths.length; i++) {
-                    let currentLoopLength = inputLengths[i];
-
-                    //console.log(getPresentableData(inputList, currentPosition, currentLoopLength) + `  --  skip = ${skipSize}`);
-
-                    let reversedSection = [];
-
-                    for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
-                        readFrom = currentPosition + x;
-
-                        if (readFrom >= inputList.length) {
-                            readFrom -= inputList.length;
-                        }
-
-                        reversedSection.unshift(inputList[readFrom]);
-                    }
-
-                    for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {                        readFrom = currentPosition + x;
-                        readFrom = currentPosition + x;
-
-                        if (readFrom >= inputList.length) {
-                            readFrom -= inputList.length;
-                        }
-
-                        inputList[readFrom] = reversedSection[x];
-                    }
-
-                    currentPosition += currentLoopLength + skipSize;
-                    skipSize++;
-
-                    if (currentPosition >= inputList.length) {
-                        currentPosition -= inputList.length;
-                    }
-                }
-
-                return inputList[0] * inputList[1];
+                let knot = tieKnot(data.list, data.lengths);
+                return knot[0] * knot[1];
             }
         },
 
         {
             title: "Puzzle 2",
-            expectedAnswer: null,
+            expectedAnswer: "e1a65bfb5a5ce396025fab5528c25a87",
             testSets: [
                 { expectedAnswer: "a2582a3a0e66e6e86e3812dcb672a272", data: "" },
                 { expectedAnswer: "33efeb34ea91902bb2f59c9920caa6cd", data: "AoC 2017" },
@@ -81,76 +104,19 @@
             getSolution: data => {
                 let currentPosition = 0;
                 let skipSize = 0;
-
                 let inputList = _.range(0,256);
-
                 let inputString = data.hasOwnProperty("lengths") ? data.lengths.join(",") : data;
-
                 let inputLengths = inputString.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
 
-                for (let round = 0; round < 64; round++) {
-                    //console.log(`Round ${round} with inputLengths ${JSON.stringify(inputLengths)} and list ${JSON.stringify(inputList)} `);
-                    //console.log(getPresentableData(inputList, currentPosition, inputLengths[0]) + `  --  skip = ${skipSize}`);
-                    //console.log("");
+                let knot = tieKnot(inputList, inputLengths, 64);
 
-                    for (let i = 0; i < inputLengths.length; i++) {
-                        let currentLoopLength = inputLengths[i];
-                        let reversedSection = [];
-
-                        for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
-                            readFrom = currentPosition + x;
-
-                            if (readFrom >= inputList.length) {
-                                readFrom -= inputList.length;
-                            }
-
-                            reversedSection.unshift(inputList[readFrom]);
-                        }
-
-                        for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {                        readFrom = currentPosition + x;
-                            readFrom = currentPosition + x;
-
-                            if (readFrom >= inputList.length) {
-                                readFrom -= inputList.length;
-                            }
-
-                            inputList[readFrom] = reversedSection[x];
-                        }
-
-                        currentPosition += currentLoopLength + skipSize;
-                        skipSize++;
-
-                        while (currentPosition >= inputList.length) {
-                            currentPosition -= inputList.length;
-                        }
-                    }
-                }
-
-                console.log(inputList); // The "Sparse Hash"
-
-                function getDenseHash(sparseHash) {
-                    let result = [];
-
-                    for (let blockNr = 0; blockNr < 16; blockNr++) {
-                        let block = sparseHash.slice(blockNr * 16, (blockNr + 1) * 16);
-                        result[blockNr] = block.reduce((a,b) => a ^ b);
-                    }
-
-                    return result;
-                }
                 // Should give only 64s:
                 //console.log(getDenseHash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]));
 
-                function getHexForDenseHash(denseHash) {
-                    return denseHash.map(digit => {
-                        let result = digit.toString(16);
-                        return result.length === 1 ? `0${result}` : result;
-                    }).join("");
-                }
                 // Should give 4007ff:
                 // console.log(getHexForDenseHash([64, 7, 255]));
 
-                return getHexForDenseHash(getDenseHash(inputList));
+                return getHexForDenseHash(getDenseHash(knot));
             }
         }]
     };
