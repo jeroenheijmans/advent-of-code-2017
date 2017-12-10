2e4a3a49eb9f7710a8185fb7818a3eed9884d151
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-10 07:21:04 +0100

    Day 10 puzzle 1

--- days/10.js
+++ days/10.js
@@ -1,17 +1,71 @@
 (function(aoc) {
+    function getPresentableData(list, currentPosition, length) {
+        let rightBoundary = currentPosition + length - 1;
+
+        if (rightBoundary >= list.length) { 
+            rightBoundary -= list.length; 
+        }
+
+        return list.map((item, idx) => 
+                idx === currentPosition && idx === rightBoundary ? `([${item}])`
+                : idx === currentPosition ? `([${item}]`
+                : idx === rightBoundary ? `${item})`
+                : `${item}`
+            ).join(" ") + `  --  currentPosisition = ${currentPosition}  --  length = ${length}`;
+    }
+
     aoc.days["10"] = {
-        actualInput: [],
+        actualInput: {list: _.range(0,256), lengths: [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110] },
 
         puzzles:[{
             title: "Puzzle 1",
-            expectedAnswer: null,
+            expectedAnswer: 23874,
             testSets: [
-                { expectedAnswer: null, data: [] },
+                { expectedAnswer: 12, data: {list: _.range(0,5), lengths: [3, 4, 1, 5]} },
             ],
             getSolution: data => {
-                var result = 0;
+                let currentPosition = 0;
+                let skipSize = 0;
 
-                return result;
+                let inputList = data.list;
+                let inputLengths = data.lengths;
+
+                for (let i = 0; i < inputLengths.length; i++) {
+                    let currentLoopLength = inputLengths[i];
+
+                    //console.log(getPresentableData(inputList, currentPosition, currentLoopLength) + `  --  skip = ${skipSize}`);
+
+                    let reversedSection = [];
+                    
+                    for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
+                        readFrom = currentPosition + x;
+                        
+                        if (readFrom >= inputList.length) {
+                            readFrom -= inputList.length;
+                        }
+
+                        reversedSection.unshift(inputList[readFrom]);
+                    }
+                    
+                    for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {                        readFrom = currentPosition + x;
+                        readFrom = currentPosition + x;
+                        
+                        if (readFrom >= inputList.length) {
+                            readFrom -= inputList.length;
+                        }
+
+                        inputList[readFrom] = reversedSection[x];
+                    }
+
+                    currentPosition += currentLoopLength + skipSize;
+                    skipSize++;
+
+                    if (currentPosition >= inputList.length) {
+                        currentPosition -= inputList.length;
+                    }
+                }
+
+                return inputList[0] * inputList[1];
             }
         },
 
