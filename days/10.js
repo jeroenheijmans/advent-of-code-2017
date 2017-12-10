350449c11484c021ec23ff80532935743714fa63
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-10 09:54:45 +0100

    Refactor day 10 some more

--- days/10.js
+++ days/10.js
@@ -20,31 +20,31 @@
         let skipSize = 0;
 
         for (let round = 0; round < rounds; round++) {
+            // For debug cases in test sets from puzzle 1:
+            // console.log(getPresentableData(result, currentPosition, currentLoopLength, skipSize));
+
             for (let i = 0; i < inputLengths.length; i++) {
                 let currentLoopLength = inputLengths[i];
-
-                // console.log(getPresentableData(result, currentPosition, currentLoopLength, skipSize));
-
                 let reversedSection = [];
 
-                for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
-                    readFrom = currentPosition + x;
+                for (let at = currentPosition, x = 0; x < currentLoopLength; x++) {
+                    at = currentPosition + x;
 
-                    if (readFrom >= result.length) {
-                        readFrom -= result.length;
+                    if (at >= result.length) {
+                        at -= result.length;
                     }
 
-                    reversedSection.unshift(result[readFrom]);
+                    reversedSection.unshift(result[at]);
                 }
 
-                for (let readFrom = currentPosition, x = 0; x < currentLoopLength; x++) {
-                    readFrom = currentPosition + x;
+                for (let at = currentPosition, x = 0; x < currentLoopLength; x++) {
+                    at = currentPosition + x;
 
-                    if (readFrom >= result.length) {
-                        readFrom -= result.length;
+                    if (at >= result.length) {
+                        at -= result.length;
                     }
 
-                    result[readFrom] = reversedSection[x];
+                    result[at] = reversedSection[x];
                 }
 
                 currentPosition += currentLoopLength + skipSize;
@@ -70,7 +70,7 @@
         return result;
     }
     
-    function getHexForDenseHash(denseHash) {
+    function getHexForArray(denseHash) {
         return denseHash.map(digit => {
             let result = digit.toString(16);
             return result.length === 1 ? `0${result}` : result;
@@ -78,16 +78,18 @@
     }
 
     aoc.days["10"] = {
-        actualInput: { list: _.range(0,256), lengths: [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110] },
+        actualInput: { list: _.range(0,256), lengths: "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110" },
 
         puzzles:[{
             title: "Puzzle 1",
             expectedAnswer: 23874,
             testSets: [
-                { expectedAnswer: 12, data: { list: _.range(0,5), lengths: [3, 4, 1, 5]} },
+                { expectedAnswer: 12, data: { list: _.range(0,5), lengths: "3,4,1,5" } },
             ],
             getSolution: data => {
-                let knot = tieKnot(data.list, data.lengths);
+                let inputLengths = data.lengths.split(",").map(c => parseInt(c, 10));
+                let knot = tieKnot(data.list, inputLengths);
+
                 return knot[0] * knot[1];
             }
         },
@@ -96,17 +98,16 @@
             title: "Puzzle 2",
             expectedAnswer: "e1a65bfb5a5ce396025fab5528c25a87",
             testSets: [
-                { expectedAnswer: "a2582a3a0e66e6e86e3812dcb672a272", data: "" },
-                { expectedAnswer: "33efeb34ea91902bb2f59c9920caa6cd", data: "AoC 2017" },
-                { expectedAnswer: "3efbe78a8d82f29979031a4aa0b16a9d", data: "1,2,3" },
-                { expectedAnswer: "63960835bcdc130f0b66d7ff4f6a5a8e", data: "1,2,4" },
+                { expectedAnswer: "a2582a3a0e66e6e86e3812dcb672a272", data: { lengths: "" } },
+                { expectedAnswer: "33efeb34ea91902bb2f59c9920caa6cd", data: { lengths: "AoC 2017" } },
+                { expectedAnswer: "3efbe78a8d82f29979031a4aa0b16a9d", data: { lengths: "1,2,3" } },
+                { expectedAnswer: "63960835bcdc130f0b66d7ff4f6a5a8e", data: { lengths: "1,2,4" } },
             ],
             getSolution: data => {
                 let currentPosition = 0;
                 let skipSize = 0;
-                let inputList = _.range(0,256);
-                let inputString = data.hasOwnProperty("lengths") ? data.lengths.join(",") : data;
-                let inputLengths = inputString.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
+                let inputList = data.list || _.range(0,256);
+                let inputLengths = data.lengths.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
 
                 let knot = tieKnot(inputList, inputLengths, 64);
 
@@ -116,7 +117,7 @@
                 // Should give 4007ff:
                 // console.log(getHexForDenseHash([64, 7, 255]));
 
-                return getHexForDenseHash(getDenseHash(knot));
+                return getHexForArray(getDenseHash(knot));
             }
         }]
     };
