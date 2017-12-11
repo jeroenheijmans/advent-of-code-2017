fbd2a98933168550348be32296762b9432208f46
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-11 07:21:13 +0100

    Prepare day 12

---
+++ days/13.js
@@ -0,0 +1,39 @@
+(function(aoc) {
+    aoc.days["13"] = {
+        actualInput: [],
+
+        puzzles:[{
+            title: "Puzzle 1",
+            expectedAnswer: null,
+            testSets: [
+                { expectedAnswer: null, data: [] },
+            ],
+            getSolution: data => {
+                var result = 0;
+
+                return result;
+            }
+        },
+
+        /*{
+            title: "Puzzle 2",
+            expectedAnswer: null,
+            testSets: [
+                { expectedAnswer: null, data: [] },
+            ],
+            getSolution: data => {
+                var result = 0;
+
+                return result;
+            }
+        }*/]
+
+        /*,bonusTests: [{
+            title: "placeholder",
+            test: assert => {
+                let result = "SOMETHING";
+                assert.strictEqual(result, "SOMETHING");
+            }
+        }]*/
+    };
+}(window.aoc = window.aoc || {days:{}}));
