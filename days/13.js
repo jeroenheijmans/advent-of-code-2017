0e68fce062fc8d492b99b871fa29f73acba5efb2
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-13 19:11:33 +0100

    Refactor day 13 puzzle 1

--- days/13.js
+++ days/13.js
@@ -1,4 +1,19 @@
 (function(aoc) {
+
+    function getFirewall(input) {
+        return input.reduce((map, entry) => {
+            map[entry[0]] = {
+                depth: entry[0],
+                range: entry[1],
+                mod: (entry[1] - 1) * 2,
+                at: 0,
+                dir: 1,
+            };
+
+            return map;
+        }, {});
+    }
+    
     aoc.days["13"] = {
         actualInput: [
             [0, 3],
@@ -59,29 +74,19 @@
              ] },
             ],
             getSolution: data => {
-                let input = data.slice(0);
-
-                let firewall = {};
-
-                for (let entry of data) {
-                    firewall[entry[0]] = {
-                        depth: entry[0],
-                        range: entry[1],
-                        at: 0,
-                        dir: 1,
-                    };
-                }
+                let firewall = getFirewall(data);
 
                 let severity = 0;
                 let maxDepth = Math.max.apply(Math, data.map(d => d[0]));
+                let scannerKeys = Object.keys(firewall);
                 
-                for (let position = 0; position < maxDepth+2; position++) {
+                for (let position = 0; position <= maxDepth; position++) {
                     if (firewall.hasOwnProperty(position) && firewall[position].at === 0) {
-                        console.log(`collision at ${position}`)
+                        // Collision!
                         severity += firewall[position].depth * firewall[position].range;
                     }
 
-                    Object.keys(firewall).forEach(k => {
+                    for (let k of scannerKeys) {
                         if (firewall[k].at === firewall[k].range - 1) {
                             firewall[k].dir = -1;
                         } else if (firewall[k].at === 0) {
@@ -89,7 +94,7 @@
                         } 
 
                         firewall[k].at += firewall[k].dir;
-                    });
+                    };
                 }
 
                 return severity;
