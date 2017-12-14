f6d8c01a08ea5231bd71af6ed12c99fe6ab8799a
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-14 08:27:21 +0100

    Day 13 puzzle 2

--- days/14.js
+++ days/14.js
@@ -60,9 +60,8 @@
 
         puzzles:[{
             title: "Puzzle 1",
-            expectedAnswer: null,
+            expectedAnswer: 8214,
             testSets: [
-                { expectedAnswer: null, data: `` },
             ],
             getSolution: data => {
                 let result = 0;
@@ -70,9 +69,8 @@
                 for (let i = 0; i < 128; i++) {
                     let input = `${data}-${i}`;
                     let hash = getKnotHash(input);
-                    console.log(hash);
-
                     let bin = convert(hash);
+
                     result += bin.split("").map(c => parseInt(c, 10)).reduce((a,b) => a+b);
                 }
 
@@ -80,18 +78,82 @@
             }
         },
 
-        /*{
+        {
             title: "Puzzle 2",
             expectedAnswer: null,
             testSets: [
-                { expectedAnswer: null, data: `` },
             ],
             getSolution: data => {
-                let input = data;
+                let grid = [];
 
-                return "NOT FOUND";
+                for (let i = 0; i < 128; i++) {
+                    let input = `${data}-${i}`;
+                    let hash = getKnotHash(input);
+                    let bin = convert(hash);
+                    
+                    let row = bin.split("").map(c => parseInt(c, 10));
+                    grid.push(row);
+                }
+
+                //grid[0] = grid[0].map(cell => cell === 1 ? 2 : cell);
+                //grid = [[1,0,1,1],[1,0,1,0],[0,0,0,0],[1,0,1,1]];
+                //grid = [[1,0,1,1],[1,1,0,0],[1,0,1,0],[0,1,0,1]];
+                //grid = [[1,1,1,1],[1,1,1,1],[1,0,0,0],[0,1,1,1]];
+                //grid = [[1,0,0,1],[0,0,0,1],[1,0,0,1],[1,1,0,1]];
+
+                // for (let i=0; i<4; i++) { console.log(`${i}  =>  ${JSON.stringify(grid[i])}`); }
+                // console.log("");
+
+                let groups = [];
+                
+                function touches(x, y, group) {
+                    return group.some(point => 
+                        (point.x === x && Math.abs(point.y - y) === 1)
+                        || (point.y === y && Math.abs(point.x - x) === 1)
+                    );
+                }
+
+                for (let y = 0; y < grid[0].length; y++) {
+                    for (let x = 0; x < grid[0].length; x++) {
+
+                        if (grid[y][x] === 1) {
+                            let group = groups.find(g => touches(x, y, g));
+
+                            if (!!group) {
+                                group.push({x:x,y:y});
+                            } else {
+                                groups.push([{x:x,y:y}]);
+                            }
+                        }                        
+                    }
+                }
+//1093?
+                for (let z=0; z<100; z++) {
+                    groups = groups.reduce((result, grp) => {
+                        let wasAppended = false;
+                        for (let i = 0; i < result.length; i++) {
+                            if (result[i].some(p => touches(p.x, p.y, grp))) {
+                                result[i] = result[i].concat(grp);
+                                wasAppended = true;
+                                break;
+                            }
+                        }
+                        if (!wasAppended) {
+                            result.push(grp);
+                        }                
+                        return result;
+                    }, []);
+                }
+
+                console.log(groups);
+            
+                // NOT 2063
+                // NOT 1141 (curiously unlucky guess)
+                // NOT 1138 (after switching x and y)
+                // AARRGGGHHH I SHOULD RETHINK MY APPROACH AND START OVER!!!
+                return groups.length;
             }
-        }*/]
+        }]
 
         ,bonusTests: [{
             title: "placeholder",
