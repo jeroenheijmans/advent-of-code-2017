6976bba27e7ff828ad1c822ca03ebfe9bbc2c259
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-14 06:33:06 +0100

    Day 14 puzzle 1

--- days/14.js
+++ days/14.js
@@ -1,6 +1,62 @@
 (function(aoc) {
+    function getKnotHash(inputText) {
+        let inputArray = inputText.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
+        let knot = tieKnot(inputArray, 64);
+        let denseHash = getDenseHash(knot);
+        let hex = getHexForArray(denseHash);
+        return hex;
+    }
+
+    function tieKnot(inputText, rounds = 1) {
+        let result = _.range(0,256);
+        let position = 0;
+        let skip = 0;
+
+        for (let round = 0; round < rounds; round++) {
+            for (let i = 0; i < inputText.length; i++) {
+                // For debug cases in test sets from puzzle 1:
+                // console.log(getPresentableData(result, position, loopLength, skip));
+
+                let loopLength = inputText[i];
+                let reversedSection = [];
+
+                for (let at = position, x = 0; x < loopLength; x++) {
+                    at = (position + x) % result.length;
+                    reversedSection.unshift(result[at]);
+                }
+
+                for (let at = position, x = 0; x < loopLength; x++) {
+                    at = (position + x) % result.length;
+                    result[at] = reversedSection[x];
+                }
+
+                position = (position + loopLength + skip) % result.length;
+                skip++;
+            }
+        }
+
+        return result;
+    }
+
+    function getDenseHash(sparseHash) {
+        let result = [];
+
+        for (let blockNr = 0; blockNr < 16; blockNr++) {
+            let block = sparseHash.slice(blockNr * 16, (blockNr + 1) * 16);
+            result[blockNr] = block.reduce((a,b) => a ^ b);
+        }
+
+        return result;
+    }
+
+    function getHexForArray(denseHash) {
+        return denseHash
+            .map(digit => ("0" + digit.toString(16)).substr(-2))
+            .join("");
+    }
+
     aoc.days["14"] = {
-        actualInput: ``,
+        actualInput: `hxtvlmkl`,
 
         puzzles:[{
             title: "Puzzle 1",
@@ -9,9 +65,18 @@
                 { expectedAnswer: null, data: `` },
             ],
             getSolution: data => {
-                let input = data;
+                let result = 0;
 
-                return "NOT FOUND";
+                for (let i = 0; i < 128; i++) {
+                    let input = `${data}-${i}`;
+                    let hash = getKnotHash(input);
+                    console.log(hash);
+
+                    let bin = convert(hash);
+                    result += bin.split("").map(c => parseInt(c, 10)).reduce((a,b) => a+b);
+                }
+
+                return result;
             }
         },
 
@@ -28,12 +93,26 @@
             }
         }*/]
 
-        /*,bonusTests: [{
+        ,bonusTests: [{
             title: "placeholder",
             test: assert => {
-                let result = "SOMETHING";
-                assert.strictEqual(result, "SOMETHING");
+                let result = convert("f");
+                assert.strictEqual(result, "1111");
             }
-        }]*/
+        },{
+            title: "placeholder",
+            test: assert => {
+                let result = convert("a0c2017");
+                assert.strictEqual(result, "1010000011000010000000010111");
+            }
+        }]
     };
+
+    function convert(input) {
+        return input
+            .split("")
+            .map(c => parseInt(c, 16).toString(2))
+            .map(s => ("000" + s).substr(-4))        
+            .join("");
+    }
 }(window.aoc = window.aoc || {days:{}}));
