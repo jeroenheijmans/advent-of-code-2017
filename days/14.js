623c425fb5e14b434e07c940b1b632304b128b93
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-14 23:35:43 +0100

    Refactor day 14 some more

--- days/14.js
+++ days/14.js
@@ -1,4 +1,25 @@
 (function(aoc) {
+    function convertToBinaryText(input) {
+        return input
+            .split("")
+            .map(c => parseInt(c, 16).toString(2))
+            .map(s => ("000" + s).substr(-4))        
+            .join("");
+    }
+
+    function getKnotHashGrid(data) {
+        let grid = [];
+        
+        for (let i = 0; i < 128; i++) {
+            let hash = aoc.getKnotHash(`${data}-${i}`);
+            let bin = convertToBinaryText(hash);
+            let row = bin.split("").map(c => parseInt(c, 10));
+            grid.push(row);
+        }
+
+        return grid;
+    }
+
     aoc.days["14"] = {
         actualInput: `hxtvlmkl`,
 
@@ -8,17 +29,8 @@
             testSets: [
             ],
             getSolution: data => {
-                let result = 0;
-
-                for (let i = 0; i < 128; i++) {
-                    let input = `${data}-${i}`;
-                    let hash = aoc.getKnotHash(input);
-                    let bin = convert(hash);
-
-                    result += bin.split("").map(c => parseInt(c, 10)).reduce((a,b) => a+b);
-                }
-
-                return result;
+                let grid = getKnotHashGrid(data);
+                return grid.reduce((a,b) => a + b.reduce((i,j) => i + j), 0);
             }
         },
 
@@ -28,16 +40,7 @@
             testSets: [
             ],
             getSolution: data => {
-                let grid = [];
-
-                for (let i = 0; i < 128; i++) {
-                    let input = `${data}-${i}`;
-                    let hash = aoc.getKnotHash(input);
-                    let bin = convert(hash);
-                    
-                    let row = bin.split("").map(c => parseInt(c, 10));
-                    grid.push(row);
-                }
+                let grid = getKnotHashGrid(data);
 
                 let result = 0;
 
@@ -68,23 +71,15 @@
         ,bonusTests: [{
             title: "placeholder",
             test: assert => {
-                let result = convert("f");
+                let result = convertToBinaryText("f");
                 assert.strictEqual(result, "1111");
             }
         },{
             title: "placeholder",
             test: assert => {
-                let result = convert("a0c2017");
+                let result = convertToBinaryText("a0c2017");
                 assert.strictEqual(result, "1010000011000010000000010111");
             }
         }]
     };
-
-    function convert(input) {
-        return input
-            .split("")
-            .map(c => parseInt(c, 16).toString(2))
-            .map(s => ("000" + s).substr(-4))        
-            .join("");
-    }
 }(window.aoc = window.aoc || {days:{}}));
