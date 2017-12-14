8a7f3b92a65f85e6ef9b73517f4c7a9f604c4f15
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-14 22:54:58 +0100

    Reuse code between day 10 and 14

--- days/10.js
+++ days/10.js
@@ -12,19 +12,19 @@
                 : idx === rightBoundary ? `${item})`
                 : `${item}`
             ).join(" ") + `  --  currentPosisition = ${position}  --  length = ${length}  --  skip = ${skip}`;
-    }
+    };
 
-    function tieKnot(input, lengths, rounds = 1) {
-        let result = input.slice();
+    aoc.tieKnot = function(inputText, rounds = 1, range = 256) {
+        let result = _.range(0, range);
         let position = 0;
         let skip = 0;
 
         for (let round = 0; round < rounds; round++) {
-            for (let i = 0; i < lengths.length; i++) {
+            for (let i = 0; i < inputText.length; i++) {
                 // For debug cases in test sets from puzzle 1:
                 // console.log(getPresentableData(result, position, loopLength, skip));
 
-                let loopLength = lengths[i];
+                let loopLength = inputText[i];
                 let reversedSection = [];
 
                 for (let at = position, x = 0; x < loopLength; x++) {
@@ -43,9 +43,9 @@
         }
 
         return result;
-    }
+    };
 
-    function getDenseHash(sparseHash) {
+    aoc.getDenseHash = function(sparseHash) {
         let result = [];
 
         for (let blockNr = 0; blockNr < 16; blockNr++) {
@@ -54,26 +54,34 @@
         }
 
         return result;
-    }
+    };
 
-    function getHexForArray(denseHash) {
+    aoc.getHexForArray = function(denseHash) {
         return denseHash
             .map(digit => ("0" + digit.toString(16)).substr(-2))
             .join("");
+    };
+
+    aoc.getKnotHash = function (inputText) {
+        let inputArray = inputText.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
+        let knot = aoc.tieKnot(inputArray, 64);
+        let denseHash = aoc.getDenseHash(knot);
+        let hex = aoc.getHexForArray(denseHash);
+        return hex;
     }
 
     aoc.days["10"] = {
-        actualInput: { list: _.range(0,256), lengths: "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110" },
+        actualInput: { range: 256, lengths: "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110" },
 
         puzzles: [{
             title: "Puzzle 1",
             expectedAnswer: 23874,
             testSets: [
-                { expectedAnswer: 12, data: { list: _.range(0,5), lengths: "3,4,1,5" } },
+                { expectedAnswer: 12, data: { range: 5, lengths: "3,4,1,5" } },
             ],
             getSolution: data => {
                 let lengths = data.lengths.split(",").map(c => parseInt(c, 10));
-                let knot = tieKnot(data.list, lengths);
+                let knot = aoc.tieKnot(lengths, 1, data.range);
 
                 return knot[0] * knot[1];
             }
@@ -91,26 +99,25 @@
             getSolution: data => {
                 let position = 0;
                 let skip = 0;
-                let input = data.list || _.range(0,256);
                 let lengths = data.lengths.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
 
-                let knot = tieKnot(input, lengths, 64);
+                let knot = aoc.tieKnot(lengths, 64);
 
-                return getHexForArray(getDenseHash(knot));
+                return aoc.getHexForArray(aoc.getDenseHash(knot));
             }
         }],
 
         bonusTests: [{
             title: "getDenseHash for example",
             test: assert => {
-                var result = getDenseHash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]);
+                var result = aoc.getDenseHash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22, 65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]);
                 var expected = _.range(0,16).map(_ => 64);
                 assert.deepEqual(result, expected);
             }
         }, {
             title: "getHexForArray for example",
             test: assert => {
-                var result = getHexForArray([64, 7, 255]);
+                var result = aoc.getHexForArray([64, 7, 255]);
                 assert.strictEqual(result, "4007ff");
             }
         }]
--- days/14.js
+++ days/14.js
@@ -1,60 +1,4 @@
 (function(aoc) {
-    function getKnotHash(inputText) {
-        let inputArray = inputText.split("").map(c => c.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
-        let knot = tieKnot(inputArray, 64);
-        let denseHash = getDenseHash(knot);
-        let hex = getHexForArray(denseHash);
-        return hex;
-    }
-
-    function tieKnot(inputText, rounds = 1) {
-        let result = _.range(0,256);
-        let position = 0;
-        let skip = 0;
-
-        for (let round = 0; round < rounds; round++) {
-            for (let i = 0; i < inputText.length; i++) {
-                // For debug cases in test sets from puzzle 1:
-                // console.log(getPresentableData(result, position, loopLength, skip));
-
-                let loopLength = inputText[i];
-                let reversedSection = [];
-
-                for (let at = position, x = 0; x < loopLength; x++) {
-                    at = (position + x) % result.length;
-                    reversedSection.unshift(result[at]);
-                }
-
-                for (let at = position, x = 0; x < loopLength; x++) {
-                    at = (position + x) % result.length;
-                    result[at] = reversedSection[x];
-                }
-
-                position = (position + loopLength + skip) % result.length;
-                skip++;
-            }
-        }
-
-        return result;
-    }
-
-    function getDenseHash(sparseHash) {
-        let result = [];
-
-        for (let blockNr = 0; blockNr < 16; blockNr++) {
-            let block = sparseHash.slice(blockNr * 16, (blockNr + 1) * 16);
-            result[blockNr] = block.reduce((a,b) => a ^ b);
-        }
-
-        return result;
-    }
-
-    function getHexForArray(denseHash) {
-        return denseHash
-            .map(digit => ("0" + digit.toString(16)).substr(-2))
-            .join("");
-    }
-
     aoc.days["14"] = {
         actualInput: `hxtvlmkl`,
 
@@ -68,7 +12,7 @@
 
                 for (let i = 0; i < 128; i++) {
                     let input = `${data}-${i}`;
-                    let hash = getKnotHash(input);
+                    let hash = aoc.getKnotHash(input);
                     let bin = convert(hash);
 
                     result += bin.split("").map(c => parseInt(c, 10)).reduce((a,b) => a+b);
@@ -88,7 +32,7 @@
 
                 for (let i = 0; i < 128; i++) {
                     let input = `${data}-${i}`;
-                    let hash = getKnotHash(input);
+                    let hash = aoc.getKnotHash(input);
                     let bin = convert(hash);
                     
                     let row = bin.split("").map(c => parseInt(c, 10));
