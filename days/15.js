84e3c62fd8b5f92398dcd622c01ed7d0dbebe92e
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-15 06:24:37 +0100

    Day 15 puzzle 1 (SLOWWWW!)

--- days/15.js
+++ days/15.js
@@ -1,17 +1,29 @@
 (function(aoc) {
     aoc.days["15"] = {
-        actualInput: ``,
+        actualInput: { a: 277, b: 349, fa: 16807, fb: 48271, reps: 40 * 1000 * 1000  },
 
         puzzles:[{
             title: "Puzzle 1",
-            expectedAnswer: null,
+            expectedAnswer: 592,
             testSets: [
-                { expectedAnswer: null, data: `` },
+                { expectedAnswer: 1, data: { a: 65, b: 8921, fa: 16807, fb: 48271, reps: 5 } },
             ],
             getSolution: data => {
                 let input = data;
+                const divisor = 2147483647;
+                let result = 0;
 
-                return "NOT FOUND";
+                for (let i=0; i<data.reps; i++) {
+                    input.a = (input.a * input.fa) % divisor;
+                    input.b = (input.b * input.fb) % divisor;
+
+                    let bina = bin(input.a);
+                    let binb = bin(input.b);
+
+                    if (bina === binb) result++;
+                }
+
+                return result;
             }
         },
 
@@ -28,12 +40,30 @@
             }
         }*/]
 
-        /*,bonusTests: [{
-            title: "placeholder",
+        ,bonusTests: [{
+            title: "Bin represtation 1",
             test: assert => {
-                let result = "SOMETHING";
-                assert.strictEqual(result, "SOMETHING");
+                let result = bin(1092455);
+                assert.strictEqual(result, "00000000000100001010101101100111");
             }
-        }]*/
+        },{
+            title: "Bin represtation 2",
+            test: assert => {
+                let result = bin(430625591);
+                assert.strictEqual(result, "00011001101010101101001100110111");
+            }
+        },{
+            title: "Bin represtation 3",
+            test: assert => {
+                let result = bin(1181022009);
+                assert.strictEqual(result, "01000110011001001111011100111001");
+            }
+        }]
     };
+
+    const mask = 00000000000000000000000000000000
+
+    function bin(input) {
+        return ("00000000000000000000000000000000" + (input >>> 0).toString(2)).substr(-16);
+    }
 }(window.aoc = window.aoc || {days:{}}));
