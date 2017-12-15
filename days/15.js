eaf5144def9415f14cfd9c4d9b77c221ee40a490
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-15 18:44:02 +0100

    Use normal binary operations

--- days/15.js
+++ days/15.js
@@ -11,15 +11,15 @@
             getSolution: data => {
                 let input = data;
                 const divisor = 2147483647;
-                const reps = data.reps || 40;//sloowww... * 1000 * 1000;
+                const reps = data.reps || 40 * 1000 * 1000;
                 let result = 0;
 
                 for (let i=0; i<reps; i++) {
                     input.a = (input.a * input.fa) % divisor;
                     input.b = (input.b * input.fb) % divisor;
 
-                    let bina = bin(input.a);
-                    let binb = bin(input.b);
+                    let bina = input.a & 0xFFFF;
+                    let binb = input.b & 0xFFFF;
 
                     if (bina === binb) result++;
                 }
@@ -38,49 +38,43 @@
                 let input = data;
                 const divisor = 2147483647;
                 let result = 0;
-                const reps = data.reps || 5 * 1000 * 1000;//sloowww... * 1000 * 1000;
+                const reps = data.reps || 5 * 1000 * 1000;
 
                 for (let i=0; i<reps; i++) {
                     do { input.a = (input.a * input.fa) % divisor; } while (input.a % 4 !== 0);
                     do { input.b = (input.b * input.fb) % divisor; } while (input.b % 8 !== 0);
 
-                    let bina = bin(input.a);
-                    let binb = bin(input.b);                  
+                    let bina = input.a & 0xFFFF;
+                    let binb = input.b & 0xFFFF;
 
                     if (bina === binb) {
                         result++;
                     }
                 }
-
-                // Not 2482, you've made a mistake!
+                
                 return result;
             }
         }]
 
+        // More or less "learning" unit tests to see how binary ops function...
         ,bonusTests: [{
             title: "Bin represtation 1",
             test: assert => {
-                let result = bin(1092455);
-                assert.strictEqual(result, "00000000000100001010101101100111");
+                let result = 1092455 & 0xFFFF;
+                assert.strictEqual(result.toString(2), "1010101101100111");
             }
         },{
             title: "Bin represtation 2",
             test: assert => {
-                let result = bin(430625591);
-                assert.strictEqual(result, "00011001101010101101001100110111");
+                let result = 430625591  & 0xFFFF;
+                assert.strictEqual(result.toString(2), "1101001100110111");
             }
         },{
             title: "Bin represtation 3",
             test: assert => {
-                let result = bin(1181022009);
-                assert.strictEqual(result, "01000110011001001111011100111001");
+                let result = 1181022009 & 0xFFFF;
+                assert.strictEqual(result.toString(2), "1111011100111001");
             }
         }]
     };
-
-    const mask = 00000000000000000000000000000000
-
-    function bin(input) {
-        return ("00000000000000000000000000000000" + (input >>> 0).toString(2)).substr(-16);
-    }
 }(window.aoc = window.aoc || {days:{}}));
