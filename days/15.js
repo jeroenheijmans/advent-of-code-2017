4d761c514b250e89464c3bdda0a8e20b0a773e72
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-15 18:56:53 +0100

    Optimze day 15 (FAST!ish)

--- days/15.js
+++ days/15.js
@@ -9,19 +9,18 @@
                 { expectedAnswer: 1, data: { a: 65, b: 8921, fa: 16807, fb: 48271, reps: 5 } },
             ],
             getSolution: data => {
-                let input = data;
                 const divisor = 2147483647;
                 const reps = data.reps || 40 * 1000 * 1000;
+                let {a,b,fa,fb} = data;
                 let result = 0;
 
-                for (let i=0; i<reps; i++) {
-                    input.a = (input.a * input.fa) % divisor;
-                    input.b = (input.b * input.fb) % divisor;
+                for (let i = 0; i < reps; i++) {
+                    a = (a * fa) % divisor;
+                    b = (b * fb) % divisor;
 
-                    let bina = input.a & 0xFFFF;
-                    let binb = input.b & 0xFFFF;
-
-                    if (bina === binb) result++;
+                    if ((a & 0xFFFF) === (b & 0xFFFF)) {
+                        result++;
+                    }
                 }
 
                 return result;
@@ -35,19 +34,16 @@
                 { expectedAnswer: 309, data: { a: 65, b: 8921, fa: 16807, fb: 48271 } },
             ],
             getSolution: data => {
-                let input = data;
                 const divisor = 2147483647;
-                let result = 0;
                 const reps = data.reps || 5 * 1000 * 1000;
+                let {a,b,fa,fb} = data;
+                let result = 0;
 
-                for (let i=0; i<reps; i++) {
-                    do { input.a = (input.a * input.fa) % divisor; } while (input.a % 4 !== 0);
-                    do { input.b = (input.b * input.fb) % divisor; } while (input.b % 8 !== 0);
+                for (let i = 0; i < reps; i++) {
+                    do { a = (a * fa) % divisor; } while ((a & 3) !== 0);
+                    do { b = (b * fb) % divisor; } while ((b & 7) !== 0);
 
-                    let bina = input.a & 0xFFFF;
-                    let binb = input.b & 0xFFFF;
-
-                    if (bina === binb) {
+                    if ((a & 0xFFFF) === (b & 0xFFFF)) {
                         result++;
                     }
                 }
